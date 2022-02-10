d98d5b8aa1969205c6ab58280cec46deef1362b7
TisheVody <trust97@icloud.com>
2022-02-11 02:32:49 +0300

    add just

---
+++ src/main/kotlin/just.kt
@@ -0,0 +1,43 @@
+class just {
+}
+
+fun main() {
+    val test = agoToText(seconds)
+    if (seconds > 60 * 60) println("был(а) в сети $test") else println("был(а) $test")
+}
+
+val seconds = 39623
+val minute = (seconds / 60)
+val hour = (minute / 60)
+val minuteWord = minuteFun(minute)
+val hourWord = hourFun(hour)
+
+fun agoToText(seconds: Int) = when (seconds) {
+        in 0..60 -> "только что"
+        in 61..60 * 60 -> "$minute $minuteWord назад"
+        in 60 * 60 + 1..24 * 60 * 60 -> "$hour $hourWord назад"
+        in 24 * 60 * 60 + 1..2 * 24 * 60 * 60 -> "сегодня"
+        in 2 * 24 * 60 * 60 + 1..3 * 24 * 60 * 60 -> "вчера"
+        in 3 * 24 * 60 * 60..Int.MAX_VALUE -> "давно"
+        else -> "неверное время"
+}
+
+fun minuteFun(minute: Int): String {
+    if (minute == 11) return "минут"
+    return when (minute % 10) {
+        1 -> "минуту"
+        2, 3, 4 -> "минуты"
+        in 5..9, 0 -> "минут"
+        else -> "неверное время"
+    }
+}
+
+fun hourFun(hour: Int): String {
+    if (hour == 11) return "часов"
+    return when (hour % 10) {
+        1 -> "час"
+        2, 3, 4 -> "часа"
+        in 5..9, 0 -> "часов"
+        else -> "неверное время"
+    }
+}
