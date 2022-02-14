49464613d523070f8ccbff21d9c22494265449e7
TisheVody <trust97@icloud.com>
2022-02-14 16:15:46 +0300

    changed different commission

--- src/main/kotlin/different commission.kt
+++ src/main/kotlin/different commission.kt
@@ -6,28 +6,33 @@ fun main() {
     val currentTransfer = 11_000_00
 
     val test = getComission(cardType, sumLastTransfers, currentTransfer)
-    println("Комиссия составит: $test копеек")
+    if (test == -1) println("Превышен лимит") else println("Комиссия составит: $test копеек")
 }
 
 fun getComission (cardType: String, sumLastTransfers: Int, currentTransfer: Int): Int {
     return when (cardType) {
-        "Maestro", "Mastercard" -> if (sumLastTransfers in 0..600_000_00)
+        "Maestro", "Mastercard" -> if (sumLastTransfers in 0..600_000_00 && currentTransfer in 0..150_000_00)
             comissionMaestroMastercard(currentTransfer)
             else -1
-        "Visa", "Мир" -> if (sumLastTransfers in 0..600_000_00)
+        "Visa", "Мир" -> if (sumLastTransfers in 0..600_000_00 && currentTransfer in 0..150_000_00)
             comissionVisaMir(currentTransfer, minCommission)
             else -1
-        "Vk Pay" -> if (sumLastTransfers in 0..40_000_00) 0 else -1
+        "Vk Pay" -> if (sumLastTransfers in 0..40_000_00 && currentTransfer in 0..15_000_00) 0 else -1
         else -> -1
     }
 }
 
 val minCommission = 35_00
-fun comissionVisaMir (currentTransfer: Int, minCommission: Int) = if ((currentTransfer * 0.0075) > minCommission)
+fun comissionVisaMir (currentTransfer: Int, minCommission: Int) =
+    if ((currentTransfer * 0.0075) > minCommission)
             (currentTransfer * 0.0075).toInt()
-            else minCommission
+        else minCommission
 
-fun comissionMaestroMastercard (currentTransfer: Int) = if (currentTransfer > 75_000_00)
+
+fun comissionMaestroMastercard (currentTransfer: Int) =
+        if (currentTransfer > 75_000_00)
             (currentTransfer * 0.006 + 20_00).toInt()
-            else 0
+        else 0
+
+
 
